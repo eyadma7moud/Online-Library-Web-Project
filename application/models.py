ee9874bd447c202244d590396b9d78c705dd5012
youssefshafey57 <20240693@stud.fci-cu.edu.eg>
2026-05-12 17:41:05 +0300

    Add Favourite model for user-book relationships

--- application/models.py
+++ application/models.py
@@ -35,3 +35,10 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    class Favourite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
+
